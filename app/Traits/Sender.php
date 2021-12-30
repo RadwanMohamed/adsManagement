73c8b400494fab2946c805156d16f03fbc28ae8f
radwan <radwan.mohamed@grandmstg.com>
2021-12-30 12:34:30 -0500

    sender

--- app/Traits/Sender.php
+++ app/Traits/Sender.php
@@ -4,7 +4,16 @@ namespace  App\Traits;
 
 use Illuminate\Support\Facades\Mail;
 
+/**
+ * Sender trait to handle external services
+ */
 trait Sender {
+    /**
+     * send mail function
+     * @param $to
+     * @param $message
+     * @param $subject
+     */
     protected  function sendEmail($to,$message,$subject){
         Mail::raw($message,function ($message)use($to,$subject){
             $message->to($to)
