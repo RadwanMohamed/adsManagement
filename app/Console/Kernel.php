dee1d7e9cc1e7324a91c3fc7e7e845116931d392
radwan <radwan.mohamed@grandmstg.com>
2021-12-30 18:08:37 -0500

    schdule daily mail

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('daily:mail')->cron('* * * * *');
+         $schedule->command('daily:mail')->dailyAt("20:00");
     }
 
     /**
